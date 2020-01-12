53bde648845b11fd00ad481f82a71c3046e37a9a
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-12 08:06:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  hash={:railroads =>{:pieces =>4, :name =>{:reading_railroad=>{},
+  hash={:railroads =>{:pieces =>4, :names =>{:reading_railroad=>{},
 	            :pennsylvania_railroad=>{},
 	            :b_and_o_railroad=>{},
 	            :shortline=>{}},:rent_in_dollars =>{:one_piece_owned=>25,
