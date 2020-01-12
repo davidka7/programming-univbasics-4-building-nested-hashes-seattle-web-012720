32eb91c2bcbdfd83493d6e42f0b63cbba5d17b71
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-12 07:56:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-  hash={:railroads =>{:pieces =>4,:railroads =>{:rent_in_dollars =>{:name =>[:reading_railroad =>{"mortgage_value"=>100}, :pennsylvania_railroad =>{"mortgage_value"=>200}, :b_and_o_railroad =>{"mortgage_value"=>400}, :shortline_railroad =>{"mortgage_value"=>800}]}}}}
+  hash={:railroads =>{:pieces =>4,:rent_in_dollars =>{:rent_in_dollars =>{:name =>[:reading_railroad =>{"mortgage_value"=>100}, :pennsylvania_railroad =>{"mortgage_value"=>200}, :b_and_o_railroad =>{"mortgage_value"=>400}, :shortline_railroad =>{"mortgage_value"=>800}]}}}}
   
   
   # When you start writing the implementation for this method, copy the Hash
